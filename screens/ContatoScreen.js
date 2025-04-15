cd6c55e5283581463168d09f02a71d4d1d337b50
NicolasBarutti <nicolasbarutti2003@gmail.com>
2025-04-15 19:48:21 -0300

    Ajuste na tela contato

--- screens/ContatoScreen.js
+++ screens/ContatoScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function ContatoScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Contato</Text>
+      <Image source={require('../assets/Contato.jpeg')} style={{ width: 300, height: 500 }}/>
     </View>
   );
 }
